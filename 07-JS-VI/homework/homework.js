f6ebc07ba1442051d44bfa2c7dcd3316a9e5986b
Juan-Sebastian-Maldonado <sebastian.maldonado@mi.unc.edu.ar>
2021-09-24 20:30:32 -0300

    Listo

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -4,17 +4,28 @@ function mayuscula(nombre) {
   //La función recibe un nombre y debe devolver el mismo que recibe pero con su primer letra en mayúscula
   //ej: Recibe "mario" ----> Devuelve "Mario"
   //Tu código:
+  var r = "";
+  var length;
+  length = nombre.length;
+  r = (nombre[0]).toUpperCase();
+  for (var i = 1 ; i < length; i++)
+  {
+    r = r + nombre[i];
+  }  
+  return r;
 }
 
 function invocarCallback(cb) {
   // Invoca al callback `cb`
   //Tu código:
+  cb();
 }
 
 function operacionMatematica(n1, n2, cb) {
   //Vamos a recibir una función que realiza una operación matemática como callback junto con dos números.
   //Devolver el callback pasándole como argumentos los números recibidos.
   //Tu código:
+  return cb(n1, n2);
 }
 
 function sumarArray(numeros, cb) {
@@ -22,12 +33,14 @@ function sumarArray(numeros, cb) {
   // Pasa el resultado a `cb`
   // No es necesario devolver nada
   //Tu código:
+  cb(numeros.reduce(function suma(acumulador, valorActual){return acumulador + valorActual;}));
 }
 
 function forEach(array, cb) {
   // Itera sobre la matriz "array" y pasa los valores al callback uno por uno
   // Pista: Estarás invocando a `cb` varias veces (una por cada valor en la matriz)
   //Tu código:
+  array.forEach(cb);
 }
 
 function map(array, cb) {
@@ -35,12 +48,27 @@ function map(array, cb) {
   // Itera sobre cada valor en "array", pásalo a `cb` y luego ubicar el valor devuelto por `cb` en un nuevo array
   // El nuevo array debe tener la misma longitud que el array del argumento
   //Tu código:
+  var new_array = array.map(cb);
+  return new_array;
+
 }
 
 function filter(array) {
   //Filtrar todos los elementos del array que comiencen con la letra "a".
   //Devolver un nuevo array con los elementos que cumplen la condición
   //Tu código:
+  function es_a(string) {
+    if(string[0] === 'a')
+    {
+      return true;
+    }
+    else
+    {
+      return false;
+    }
+  }
+  var new_array = array.filter(es_a);
+  return new_array;
 }
 
 // No modificar nada debajo de esta línea
